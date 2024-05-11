5618fdaf8c5277feca1c69bf882602080b85819c
choiharam <altitudinem@gmail.com>
2024-05-11 03:53:03 +0200

    fix basepath

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 
 const nextConfig = {
-  basePath: '/test_web_exh', // remove on prod. build
+  basePath: '/studiesofchange_web', // remove on prod. build
   reactStrictMode: true,
   output: 'export',
   images:{
