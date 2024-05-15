5494d35187caf7d42690cedb9fdb3231e621ff4c
choiharam <altitudinem@gmail.com>
2024-05-15 08:02:10 +0200

    fix year main page

--- src/app/_components/text-preview.tsx
+++ src/app/_components/text-preview.tsx
@@ -18,9 +18,12 @@ export function TextPreview({
   return (
     <div>
       <Link href={`/texts/${slug}`} className="hover:underline">
-        <h3 className="text-3xl mb-3 leading-snug">
-          {title}({year})
+        <h3 className="text-3xl leading-snug">
+          {title}
         </h3>
+        <h4 className="text-xl mb-3 leading-snug">
+          {year}
+        </h4>
         <p className="text-xl leading-relaxed mb-4">{author}</p>
         <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
       </Link>
