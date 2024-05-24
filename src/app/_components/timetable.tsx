7801090f51efd8bd8342a12fdcdda081758a7908
choiharam <altitudinem@gmail.com>
2024-05-25 01:05:50 +0200

    disable wip timetable

--- src/app/_components/timetable.tsx
+++ src/app/_components/timetable.tsx
@@ -1,19 +1,19 @@
-import { Text } from "@/interfaces/text";
-import { TextPreview } from "./text-preview";
-import { Collapsed } from "./collapsed";
-import { performances } from "./performance"
+// import { Text } from "@/interfaces/text";
+// import { TextPreview } from "./text-preview";
+// import { Collapsed } from "./collapsed";
+// import { performances } from "./performance"
 
 
 
-export function Timetable() {
-  return (
-    <section>
-      <h2 className="mb-8 text-5xl md:text-7xl leading-tight">
-        Performance
-      </h2>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-y-10 md:gap-y-20 mb-32">
-        
-      </div>
-    </section>
-  );
-}
+// export function Timetable() {
+//   return (
+//     <section>
+//       <h2 className="mb-8 text-5xl md:text-7xl leading-tight">
+//         Performance
+//       </h2>
+//       <div className="grid grid-cols-2 md:grid-cols-4 gap-y-10 md:gap-y-20 mb-32">
+
+//       </div>
+//     </section>
+//   );
+// }
